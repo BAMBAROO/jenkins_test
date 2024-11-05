d42d3abd69f754f20ee17621ce93631439034450
bryan athallah <sandworm@rimbahouse.com>
2024-11-05 11:32:30 +0700

    prubahan7

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">Abouggjjsdasd</RouterLink>
+        <RouterLink to="/about">Abouggjjsd</RouterLink>
       </nav>
     </div>
   </header>
