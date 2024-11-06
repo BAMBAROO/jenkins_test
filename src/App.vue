f48397db9876140060efcff9c42784e7532e02a8
bryan athallah <sandworm@rimbahouse.com>
2024-11-06 14:44:01 +0700

    joker

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">batman</RouterLink>
+        <RouterLink to="/about">joker</RouterLink>
       </nav>
     </div>
   </header>
