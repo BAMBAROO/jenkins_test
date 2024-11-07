ec386acf019aa3c739bf7ca767f1bf7abed7d58e
bryan athallah <sandworm@rimbahouse.com>
2024-11-07 10:28:10 +0700

    culprit

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">victim</RouterLink>
+        <RouterLink to="/about">culprit</RouterLink>
       </nav>
     </div>
   </header>
