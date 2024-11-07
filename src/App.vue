670c1773b82008d4f7b40ea9480bc1425275ac3a
bryan athallah <sandworm@rimbahouse.com>
2024-11-07 10:45:38 +0700

    bau

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">kentut</RouterLink>
+        <RouterLink to="/about">bau</RouterLink>
       </nav>
     </div>
   </header>
