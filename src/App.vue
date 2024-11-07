3655873c21d1837054a65506217faedc8ca452fe
Bryan Athallah <109186410+BAMBAROO@users.noreply.github.com>
2024-11-07 09:57:09 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">hero</RouterLink>
+        <RouterLink to="/about">hunter</RouterLink>
       </nav>
     </div>
   </header>
