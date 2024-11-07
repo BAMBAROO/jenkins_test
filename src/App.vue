2b41672d629c0051bf01d713242e1307138e19ea
bryan athallah <sandworm@rimbahouse.com>
2024-11-07 10:34:15 +0700

    kentut

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">hunter</RouterLink>
+        <RouterLink to="/about">kentut</RouterLink>
       </nav>
     </div>
   </header>
