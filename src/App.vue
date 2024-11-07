ac078d02b907cdfc66cf8877b300b53c832430bd
bryan athallah <sandworm@rimbahouse.com>
2024-11-07 10:22:32 +0700

    victim

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">innocent</RouterLink>
+        <RouterLink to="/about">victim</RouterLink>
       </nav>
     </div>
   </header>
