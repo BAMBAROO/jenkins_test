4ce976d1b288a172dc5128bf4aba6e168a24955f
bryan athallah <sandworm@rimbahouse.com>
2024-11-07 10:52:09 +0700

    angel

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">bau</RouterLink>
+        <RouterLink to="/about">angel</RouterLink>
       </nav>
     </div>
   </header>
